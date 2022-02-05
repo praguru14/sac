b8981981f9c814afd87e40553aac1bea489fbe04
Prafull Gururani <praguru14@gmail.com>
2022-02-05 16:21:44 +0530

    Refactored

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+function findavg() {
+    var n1 = document.getElementById("num1").value;
+    var n2 = document.getElementById("num2").value;
+
+    var nenum1 = Number(n1);
+    var nenum2 = Number(n2);
+
+    var avg = parseFloat((nenum1 + nenum2) / 2);
+
+    var anz = parseint(avg);
+
+    document.getElementById("Youranswer").innerHTML = "Average is : " + anz;
+}
