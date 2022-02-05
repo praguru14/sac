f008662e345965bfbbcd19b87d8ca8cb4de0590f
Prafull Gururani <praguru14@gmail.com>
2022-02-05 17:05:48 +0530

    Rest for sachin to do

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,35 @@
 console.log("hello");
 
 function findavg() {
-    var n1 = document.getElementById("num1").value;
-    var n2 = document.getElementById("num2").value;
 
-    var nenum1 = Number(n1);
-    var nenum2 = Number(n2);
+    //TODO: Refactor this let 
+    let n1 = Number(document.getElementById("num1").value);
+    let n2 = Number(document.getElementById("num2").value);
+    let n3 = Number(document.getElementById("num3").value);
+    let n4 = Number(document.getElementById("num4").value);
+    let n5 = Number(document.getElementById("num5").value);
+    let n6 = Number(document.getElementById("num6").value);
+    let n7 = Number(document.getElementById("num7").value);
+    let n8 = Number(document.getElementById("num8").value);
+    let n9 = Number(document.getElementById("num9").value);
+    let n10 = Number(document.getElementById("num10").value);
+    let n11 = Number(document.getElementById("num11").value);
+    let n12 = Number(document.getElementById("num12").value);
+    let n13 = Number(document.getElementById("num13").value);
+    let n14 = Number(document.getElementById("num14").value);
+    let n15 = Number(document.getElementById("num15").value);
+    let n16 = Number(document.getElementById("num16").value);
+    let n17 = Number(document.getElementById("num17").value);
+    let n18 = Number(document.getElementById("num18").value);
+    let n19 = Number(document.getElementById("num19").value);
+    let n20 = Number(document.getElementById("num20").value);
 
-    var avg = parseFloat((nenum1 + nenum2) / 2);
 
-    var anz = parseint(avg);
 
-    document.getElementById("Youranswer").innerHTML = "Average is : " + anz;
+
+    // let avg = parseFloat((nenum1 + nenum2) / 2);
+
+    // let anz = parseInt(avg);
+
+    // document.getElementById("Youranswer").innerHTML = "Average is : " + anz;
 }
