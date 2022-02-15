7361b28a0822386360336570fa5d9ee3d12bfa89
Sachin bhatt <78244834+sach14@users.noreply.github.com>
2022-02-15 13:44:19 +0530

    logic builded but not working will do after

--- JS/bmi.js
+++ JS/bmi.js
@@ -4,3 +4,17 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+function bmical()
+{
+  let weight = Number(document.getElementById("weight").value);
+  let height = Number(document.getElementById("height").value);
+  //converting height into feet
+  height = height * 12;
+  //converting feet into meters 
+  height = height * 0.3048;
+  
+  var bmi = weight / Math.pow(height,2);
+  bmi = Math.round(bmi);
+  if (bmi)
+  document.getElementById("bmi").value = +bmi;
+}
